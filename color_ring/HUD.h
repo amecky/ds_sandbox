e80421b82bca9e562d2d57c457ee7be9f65267da
Andreas Mecky <a.mecky@verband.creditreform.de>
2018-07-27 14:59:41 +0200

    score

--- color_ring/HUD.h
+++ color_ring/HUD.h
@@ -6,5 +6,9 @@ class HUD {
 
 
 public:
+	HUD();
 	void render(const std::unique_ptr<SpriteBatchBuffer>& sprites);
+private: 
+	void drawNumber(const std::unique_ptr<SpriteBatchBuffer>& sprites,const ds::vec2& pos, int value, int digits);
+	int _score;
 };
